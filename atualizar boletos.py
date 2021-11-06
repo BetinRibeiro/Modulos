2f84b51a306e419bb4e2d8ee4a073d98118a58b6
Rogoberto Ribeiro <usbetin@gmail.com>
2021-11-06 01:38:53 -0300

    atualização dos bolestos

---
+++ atualizar boletos.py
@@ -0,0 +1,31 @@
+import webbrowser
+from time import sleep
+a=0
+b=-2
+while b>-18:
+   # print ("Atualizou "+str(a)+" DE 5.600")
+#    if b<400:
+#          webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/1/'+str(b)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/2/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/3/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/4/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/5/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_cliente/6/'+str(b)+'')
+
+
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/1/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/2/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/3/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/4/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/5/'+str(a)+'')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_boletos/6/'+str(a)+'')
+   a+=100
+   b-=1
+
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/1/'+str(b)+'/0')
+   webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/2/'+str(b)+'/0')
+   webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/3/'+str(b)+'/0')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/4/'+str(b)+'/0')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/5/'+str(b)+'/0')
+   # webbrowser.open('https://rogoberto.pythonanywhere.com/agrosystem/admin_geral/lista_recebimento/6/'+str(b)+'/0')
+   sleep(30)    
